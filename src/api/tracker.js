db000ee514114bc2909c6828b840404a4903ddf8
Melik Fırat Başçi <70327175+MelikFiratBasci@users.noreply.github.com>
2023-08-30 18:47:04 +0300

    api binded signup implemented

---
+++ src/api/tracker.js
@@ -0,0 +1,7 @@
+import axios from 'axios';
+
+
+
+export default axios.create({
+    baseURL: 'https://b5ab-178-251-45-178.ngrok-free.app' //ngrok http 3000 
+});
--- src/screens/SignupScreen.js
+++ src/screens/SignupScreen.js
@@ -1,60 +1,64 @@
-import React, {useState} from 'react';
-import { View, StyleSheet} from 'react-native'
-import {Text, Input, Button} from 'react-native-elements'
-import Spacer from '../components/Spacer';
+import React, { useState, useContext } from "react";
+import { View, StyleSheet } from "react-native";
+import { Text, Input, Button } from "react-native-elements";
+import Spacer from "../components/Spacer";
+import { Context as AuthContext } from "../context/AuthContext";
 
 const SignupScreen = ({ navigation }) => {
-    const [email, setEmail] = useState('');
-    const [password,setPassword] = useState('')
+  const { state, signup } = useContext(AuthContext);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
+  console.log(state.errorMessage);
 
-
-    return (
-        //spacer componentste !! sadece aralık bırakmak için 
-        <View style ={styles.container}>
-            <Spacer>
-            <Text h2>Sign Up For Tracker</Text>
-            </Spacer>
-            <Input 
-            label ="Email" 
-            value={email} 
-            onChangeText={(newEmail)=> setEmail(newEmail) } 
-            autoCapitalize='none'
-            autoCorrect= {false}
-            /> 
-            {/*buraya doğrudan onChangeText ={setEmail} useStatesini çağırabiliriz */}
-            <Spacer />
-            <Input 
-            secureTextEntry 
-            label ="Password" 
-            value ={password} 
-            onChangeText={setPassword} 
-            autoCapitalize='none'
-            autoCorrect= {false}
-
-            />
-            <Spacer />
-            <Spacer />
-            <Button title ="Sign Up" /> 
-        </View>
-    )
-}
+  return (
+    //spacer componentste !! sadece aralık bırakmak için
+    <View style={styles.container}>
+      <Spacer>
+        <Text h2>Sign Up For Tracker</Text>
+      </Spacer>
+      <Input
+        label="Email"
+        value={email}
+        onChangeText={(newEmail) => setEmail(newEmail)}
+        autoCapitalize="none"
+        autoCorrect={false}
+      />
+      {/*buraya doğrudan onChangeText ={setEmail} useStatesini çağırabiliriz */}
+      <Spacer />
+      <Input
+        secureTextEntry
+        label="Password"
+        value={password}
+        onChangeText={setPassword}
+        autoCapitalize="none"
+        autoCorrect={false}
+      />
+      {state.errorMessage ? <Text style = {styles.errorMessage} >{state.errorMessage} </Text> : null}
+      <Spacer />
+      <Button title="Sign Up" onPress={() => signup({ email, password })} />
+    </View>
+  );
+};
 
 SignupScreen.navigationOptions = () => {
-    return {
-      headerShown: false,
-    };
+  return {
+    headerShown: false,
   };
-
-
+};
 
 const styles = StyleSheet.create({
-    container : {
-     
-        flex : 1,
-        justifyContent : 'center',
-        marginBottom :200
-    }
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    marginBottom: 200,
+  },
+  errorMessage : {
+    fontSize : 16,
+    color : 'red',
+    marginLeft : 15,
+    marginTop : 15
+  }
 });
 
 export default SignupScreen;
